e8cddfe0ae8e8c9a8c277f410c91873b97e0eca4
Josh Wolfe <thejoshwolfe@gmail.com>
2014-07-23 22:29:03 -0700

    simplifying loading pattern

--- a.js
+++ a.js
@@ -1,37 +1,39 @@
-var dumpPre;
-window.addEventListener("load", function() {
-  var document = window.document;
-  dumpPre = document.getElementById("dump");
-  document.addEventListener("keydown", function(event) {
-    if (event.ctrlKey || event.altKey) return;
-    var shouldPreventDefault = true;
-    var char = String.fromCharCode(event.which);
-    if ("1" <= char && char <= "9") {
-      roll(parseInt(char));
-    } else if (char === " ") {
-      roll(3);
-    } else {
-      shouldPreventDefault = false;
-    }
-    if (shouldPreventDefault) event.preventDefault();
-  });
-  function roll(n) {
-    var result = [];
-    var sum = 0;
-    for (var i = 0; i < n; i++) {
-      var value = randint(1, 6);
-      result.push(value);
-      sum += value;
-    }
-    result.sort();
-    var line = sum + " = " + result.join("+");
-    println(line);
+var document = window.document;
+var dumpPre = document.getElementById("dump");
+document.addEventListener("keydown", function(event) {
+  if (event.ctrlKey || event.altKey) return;
+  var shouldPreventDefault = true;
+  var char = String.fromCharCode(event.which);
+  if ("1" <= char && char <= "9") {
+    roll(parseInt(char));
+  } else if (char === " ") {
+    roll(3);
+  } else {
+    shouldPreventDefault = false;
   }
-  function randint(lower, upper) {
-    return Math.floor(Math.random() * (1 + upper - lower)) + lower;
-  }
-  function println(text) {
-    dumpPre.textContent += text + "\n";
-    dumpPre.scrollTop = dumpPre.scrollHeight;
-  }
+  if (shouldPreventDefault) event.preventDefault();
 });
+var spacebareToggle = ["block", "none"];
+document.getElementById("spacebare").addEventListener("mousedown", function flyingSpacebare() {
+  document.getElementById("space-bare").style.display = spacebareToggle[0];
+  spacebareToggle.reverse();
+});
+function roll(n) {
+  var result = [];
+  var sum = 0;
+  for (var i = 0; i < n; i++) {
+    var value = randint(1, 6);
+    result.push(value);
+    sum += value;
+  }
+  result.sort();
+  var line = sum + " = " + result.join("+");
+  println(line);
+}
+function randint(lower, upper) {
+  return Math.floor(Math.random() * (1 + upper - lower)) + lower;
+}
+function println(text) {
+  dumpPre.textContent += text + "\n";
+  dumpPre.scrollTop = dumpPre.scrollHeight;
+}
